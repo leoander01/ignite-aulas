e3dffeb6e059ee8cb38af1847c4ffc12e285b48b
Leoander Neves <44913085+leoander01@users.noreply.github.com>
2021-08-03 21:37:11 -0300

    Ouvindo mais eventos

--- ignews/src/pages/api/subscribe.ts
+++ ignews/src/pages/api/subscribe.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/client';
 import { query as q } from 'faunadb';
-import { fauna } from '../../services/fauna'
+import { fauna } from '../../services/fauna';
 import { stripe } from '../../services/stripe';
 
 type User = {
